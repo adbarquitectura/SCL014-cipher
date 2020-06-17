ca8ddecc74e01fa9335e824a997ce196f61bdc8d
Amanda <adnamadiaz@gmail.com>
2020-06-17 15:15:49 -0400

    primer repositorio con cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,43 @@
-const cipher = {
-  // ...
+const cipher = (texto, desplaza) => {
+  let textoCifra = texto.value;
+  let textCap = textoCifra.toUpperCase();
+  let desp = parseInt (desplaza.value);
+  let arrayBasic=Array.from(textCap);
+  let length = arrayBasic.length;
+  let encodeResult = ""; 
+  for(let i=0; i<length; i++){
+      let posicionArray= arrayBasic[i];
+      //console.log(posicionArray);
+      let valueTextPosition=posicionArray.charCodeAt(0);
+      //console.log(valueTextPosition);
+      let newValueTextPosition= ((valueTextPosition-65+desp)%26+65);
+      //console.log(newValueTextPosition);
+      let newV= String.fromCharCode(newValueTextPosition);
+      //console.log(newV);
+      encodeResult = encodeResult + newV; 
+      //console.log(encodeResult);
+    }
+  return encodeResult;
 };
 
-export default cipher;
+
+const deCipher = (texto, desplaza) => {
+  let textoCifra = texto.value;
+  let textCap = textoCifra.toUpperCase();
+  let desp = parseInt (desplaza.value);
+  let arrayBasic=Array.from(textCap);
+  let length = arrayBasic.length;
+  let encodeResultSec = ""; 
+  for(let i=0; i<length; i++){
+      let posicionArray= arrayBasic[i];
+      let valueTextPosition=posicionArray.charCodeAt(0);
+      let newValueTextPositionSec= valueTextPosition-64;
+      let originalValueTextPosition= (90-((26-newValueTextPositionSec+desp)%26));
+      let newVSec= String.fromCharCode(originalValueTextPosition);
+      encodeResultSec = encodeResultSec + newVSec; 
+      
+    }
+  return encodeResultSec;
+};
+
+export {cipher,deCipher};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-import cipher from './cipher.js';
+import {cipher,deCipher} from './cipher.js';
 
-console.log(cipher);
+
+let textModific = document.getElementById("text-cipher");
+//console.log(textModific);
+
+let offsetText = document.getElementById("offsetText");
+//console.log(offsetText);
+
+let screenWelcome = document.getElementById("screenWelcome");
+let screenCipher = document.getElementById("screenCipher");
+
+
+
+function Ingresar (){
+    let buttonInit = document.getElementById("ingresar");
+    buttonInit.addEventListener("click", function(){
+        screenWelcome.style.display = "none";
+        screenCipher.style.display = "block";
+    });
+};
+Ingresar();
+
+
+
+function initial (){
+    let buttonCipher = document.getElementById("btnCipher");
+    buttonCipher.addEventListener("click", printCipher);
+};
+initial();
+
+function initialSec (){
+    let buttonDeCipher = document.getElementById("btnDecipher");
+    buttonDeCipher.addEventListener("click", printDeCipher);
+};
+initialSec();
+
+function printCipher (){
+    let cipherImpre = cipher(textModific,offsetText);
+    //console.log(cipherImpre);
+    textModific.value = cipherImpre;
+    //let screenPrintOne = document.getElementById("offsetBox-cipher").style="display: block;";
+    //let ver = document.getElementById("text-return-cipher").innerHTML = "Su texto cifrado es " + cipherImpre;
+};
+
+function printDeCipher (){
+    let deCipherImpre = deCipher(textModific,offsetText);
+    textModific.value = deCipherImpre;
+    //let screenPrintOne = document.getElementById("offsetBox-deCipher").style="display: block;";
+    //let ver = document.getElementById("text-return-deCipher").innerHTML = "Su texto descifrado es " + deCipherImpre;
+};
+
+function deleteText (){
+    let buttonDeleteText = document.getElementById("btn-delete-text");
+    buttonDeleteText.addEventListener("click", function(){
+        textModific.value = "";   
+    });
+};
+deleteText();
